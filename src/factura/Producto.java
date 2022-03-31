155f2993ed97894ce317f81f7a22f50df7f2b30f
alumno_mañana <alumno_maana@PC14>
2022-03-31 13:55:38 +0200

    Ultimo commit con todos los tests y clases terminados

--- src/factura/Producto.java
+++ src/factura/Producto.java
@@ -28,4 +28,28 @@ public class Producto {
 		builder.append("]");
 		return builder.toString();
 	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public float getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
 } 
--- src/factura/ProductoTest.java
+++ src/factura/ProductoTest.java
@@ -9,7 +9,24 @@ class ProductoTest {
 
 	@Test
 	void testNuevo() {
-		
+		Producto p = new Producto ("Libro", 15F, 2);
+		String nombreActual = p.getNombre();
+		String nombreExpected = "Libro";
+		float precioActual = p.getPrecio();
+		float precioExpected = 15F;
+		int cantidadActual = p.getCantidad();
+		int cantidadExpected = 2;
+		assertEquals(nombreExpected, nombreActual);
+		assertEquals(precioExpected, precioActual);
+		assertEquals(cantidadExpected, cantidadActual);
+	}
+	
+	@Test
+	void testPrecioTotal() {
+		Producto p = new Producto ("Libro", 15F, 2);
+		float actual = p.precioTotal();
+		float expected = 30F;
+		assertEquals(actual, expected);
 	}
 
 }
