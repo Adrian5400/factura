d94ea94aa1283da54ee29e9b8939575a6d88566b
alumno_mañana <alumno_maana@PC14>
2022-03-31 12:56:05 +0200

    Commit con las clases Factura y Producto terminadas

---
+++ src/factura/Factura.java
@@ -0,0 +1,26 @@
+package factura;
+
+import java.util.Vector;
+
+public class Factura {
+	
+	Vector<Producto> productos;
+	
+	public void meterProducto(Producto p) {
+		productos.add(p);
+	}
+	
+	public float totalFactura() {
+		float precio = 0;
+		for(int i = 0; i < productos.size(); i++) {
+			precio = precio + productos.get(i).precioTotal();
+		}
+		return precio;
+	}
+	
+	public float aplicarIva(float iva) {
+		float precio = totalFactura();
+		precio = precio + (precio * iva);
+		return precio;
+	}
+}
---
+++ src/factura/Producto.java
@@ -0,0 +1,31 @@
+package factura;
+
+public class Producto {
+	private String nombre;
+	private float precio;
+	private int cantidad;
+	
+	public Producto(String nombre, float precio, int cantidad) {
+		this.nombre = nombre;
+		this.precio = precio;
+		this.cantidad = cantidad;
+	}
+	
+	public float precioTotal() {
+		precio = precio * cantidad;
+		return precio;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Producto [nombre=");
+		builder.append(nombre);
+		builder.append(", precio=");
+		builder.append(precio);
+		builder.append(", cantidad=");
+		builder.append(cantidad);
+		builder.append("]");
+		return builder.toString();
+	}
+} 
